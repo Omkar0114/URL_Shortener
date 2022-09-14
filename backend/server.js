0b96502d661c10bd4bd92503721bcd04ca292ec6
Omkar0114 <88308267+Omkar0114@users.noreply.github.com>
2022-09-14 18:39:50 +0530

    Basic server setup done

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+ require('dotenv').config()
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server running!');
+})
+
+
+app.listen(PORT, () => {
+    console.log('server is running on ' + PORT)
+})
